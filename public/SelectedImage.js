867f5573ab1a24f374b668a97b900889adc4b286
TheTopNugget1 <126112786+TheTopNugget1@users.noreply.github.com>
2024-09-27 16:46:19 +1200

    Delete public directory

--- public/SelectedImage.js
+++
@@ -1,3 +0,0 @@
-
-export const ChosenImage = "char3.png";
-export const ChosenBlock = ["bcl4.png", "bcld4.png", "bcr4.png", "bcrd4.png", "Door4.png", "flat4.png", "flipped4.png", "float4.png", "gank4.png", "ice4.png", "move4.png", "Qack4.png", "spikes4.png"];
